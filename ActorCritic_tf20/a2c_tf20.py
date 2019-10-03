aa55d613fe1b072d23b0bfa5a923f5cecddace14
Kim Myeong Seop <kimmyungsup57@gmail.com>
2019-10-03 17:39:35 +0900

    Update a2c_tf20.py

--- ActorCritic_tf20/a2c_tf20.py
+++ ActorCritic_tf20/a2c_tf20.py
@@ -11,8 +11,8 @@ num_action = env.action_space.n
 class ActorModel(Model):
     def __init__(self):
         super(ActorModel, self).__init__()
-        self.layer_a1 = Dense(128, activation='relu')
-        self.layer_a2 = Dense(128, activation='relu')
+        self.layer_a1 = Dense(64, activation='relu')
+        self.layer_a2 = Dense(64, activation='relu')
         self.logits = Dense(num_action, activation='softmax')
 
     def call(self, state):
@@ -24,8 +24,8 @@ class ActorModel(Model):
 class CriticModel(Model):
     def __init__(self):
         super(CriticModel, self).__init__()
-        self.layer_c1 = Dense(128, activation='relu')
-        self.layer_c2 = Dense(128, activation='relu')
+        self.layer_c1 = Dense(64, activation='relu')
+        self.layer_c2 = Dense(64, activation='relu')
         self.value = Dense(1)
 
     def call(self, state):
@@ -44,10 +44,10 @@ class ActorCriticTrain:
         self.en = 0.001
 
         self.actor_model = ActorModel()
-        self.actor_opt = optimizers.RMSprop(lr=self.lr, )
+        self.actor_opt = optimizers.Adam(lr=self.lr, )
 
         self.critic_model = CriticModel()
-        self.critic_opt = optimizers.RMSprop(lr=self.lr2, )
+        self.critic_opt = optimizers.Adam(lr=self.lr2, )
 
         # tensorboard
         self.log_dir = 'logs/'
@@ -56,22 +56,10 @@ class ActorCriticTrain:
         #self.train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
         #self.train_loss_c = tf.keras.metrics.Mean('train_loss_c', dtype=tf.float32)
 
-    def actor_loss(self, states, actions, values, rewards, next_values, dones):
+    def actor_loss(self, states, actions, advantages):
         policy = self.actor_model(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32))
-        advantages = []
-        for i in range(len(states)):
-            reward = np.array(rewards[i])
-            value = np.array(values[i])
-            next_value = np.array(next_values[i])
 
-            if dones[i]:
-                advantages.append(reward - value)
-            else:
-                advantages.append(reward + self.df * next_value - value)
-        advantages = tf.reshape(advantages, [len(states)])
-        tf.convert_to_tensor(advantages, dtype=tf.float32)
-
-        # SparseCategoricalCrossentropy
+        # SparseCategoricalCrossentropy = ce loss with not one-hot encoded output
         # from_logits = True  =>  cross_entropy with soft_max
         entropy = losses.categorical_crossentropy(policy, policy, from_logits=False)
         ce_loss = losses.SparseCategoricalCrossentropy(from_logits=False)
@@ -100,7 +88,6 @@ class ActorCriticTrain:
         return error
 
     def train(self, states, actions, rewards, next_states, dones):
-        advantages, values, next_values = self.compute_advantages_value(states, next_states, rewards, dones)
 
         critic_variable = self.critic_model.trainable_variables
         with tf.GradientTape() as tape_critic:
@@ -111,40 +98,35 @@ class ActorCriticTrain:
         critic_grads = tape_critic.gradient(critic_loss, critic_variable)
         self.critic_opt.apply_gradients(zip(critic_grads, critic_variable))
 
+
+        advantages = self.compute_advantages(states, rewards, dones)
         actor_variable = self.actor_model.trainable_variables
         with tf.GradientTape() as tape:
             tape.watch(actor_variable)
-            actor_loss = self.actor_loss(states, actions, values, rewards, next_values, dones)
+            actor_loss = self.actor_loss(states, actions, advantages)
 
         actor_grads = tape.gradient(actor_loss, actor_variable)
         self.actor_opt.apply_gradients(zip(actor_grads, actor_variable))
 
-        #self.train_loss(tf.reduce_mean(actor_loss))
+        # self.train_loss(tf.reduce_mean(actor_loss))
         # self.train_loss_c(tf.reduce_mean(critic_loss))
         self.train_loss = tf.reduce_mean(actor_loss)
         self.train_loss_c = tf.reduce_mean(critic_loss)
 
-    def compute_advantages_value(self, states, next_states, rewards, dones):
-        advantages = []
-        value_target = []
-        values = []
-        next_values = []
-        for i in range(len(states)):
-            state = np.array(states[i])
-            next_state = np.array(next_states[i])
-            value = self.critic_model(tf.convert_to_tensor(state[None, :], dtype=tf.float32))
-            next_value = self.critic_model(tf.convert_to_tensor(next_state[None, :], dtype=tf.float32))
-            reward = np.array(rewards[i])
-            if dones[i] :
-                advantages.append(reward - value)
-                value_target.append(reward)
-            else :
-                advantages.append(reward + self.df * next_value - value)
-                value_target.append(reward + self.df * next_value)
-            values.append(value)
-            next_values.append(next_value)
-        advantages = np.reshape(advantages, [len(states)])
-        return advantages, values, next_values
+    def compute_advantages(self, states, rewards, dones):
+        last_state = states[-1]
+        if dones[-1] == True:
+            reward_sum = 0
+        else:
+            reward_sum = self.critic_model(tf.convert_to_tensor(last_state[None, :], dtype=tf.float32))
+        discounted_rewards = []
+        for reward in rewards[::-1]:
+            reward_sum = reward + self.df * reward_sum
+            discounted_rewards.append(reward_sum)
+        discounted_rewards.reverse()
+        values = self.critic_model(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32))
+        advantages = discounted_rewards - values
+        return advantages
 
     def run(self):
 
@@ -170,6 +152,7 @@ class ActorCriticTrain:
                 #env.render()
                 if t == t_end :
                     done = True
+                    reward += 10
                 if t < t_end and done :
                     reward = -1
 
@@ -184,7 +167,7 @@ class ActorCriticTrain:
                 state = next_state
 
 
-                if len(states) == train_size or done:
+                if len(states) == train_size or done :
                     self.train(states, actions, rewards, next_states, dones)
                     states = []
                     actions = []
@@ -197,7 +180,7 @@ class ActorCriticTrain:
                     print("e : ", e, " reward : ", total_reward, " step : ", t)
                     env.reset()
                     with self.train_summary_writer.as_default():
-                        # tf.summary.scalar('actor_loss', self.train_loss.result(), step=e)
+                        # tf.summary.scalar('reward', self.reward_board.result(), step=e)
                         tf.summary.scalar('actor_loss', self.train_loss, step=e)
                         tf.summary.scalar('critic_loss', self.train_loss_c, step=e)
                         tf.summary.scalar('reward', total_reward, step=e)
